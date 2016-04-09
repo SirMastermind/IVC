09eb7041b35ea5cf80f6edd198173afd2b7e53be
Tomás Alves <sir.mastermind94@gmail.com>
2016-04-09 22:14:23 +0100

    Script's structure and processing
    Applies Otsu’s threshold and finds the areas of the objects of interest.

---
+++ first/proj_pt1.m
@@ -0,0 +1,87 @@
+clear all;
+close all;
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%% Computations %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+% Load image
+image = imread('Moedas1.jpg');
+% imshow(image); title('Original');
+
+%Convert image to gray
+image_gray = rgb2gray(image);
+
+% Use Otsu to get the threshold
+thr = graythresh(image_gray)*255;
+bw = image_gray>thr;
+% figure,imshow(bw);
+
+% Use closure to get the shapes well defined
+se = strel('disk', 5);
+bw_final = imclose(bw,se);
+% figure,imshow(bw_final);
+
+% Find and label the different regions
+[lb, num]= bwlabel(bw_final);
+% figure, imshow(mat2gray(lb));
+
+% Get the area of each object
+stats = regionprops(lb);
+objects = [stats.Area];
+objects = objects(objects > 1); % Take out single trash pixels
+    
+% Compute area for each region 
+   
+% Compute centroid for each region
+    
+% Compute perimeter for each region
+    
+% Compute distance for each object to other
+    
+% Compute differences between objects to find similarity
+    
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%% Input and program's flow %%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+disp('Welcome to the first project!');
+disp(' ');
+out = false;
+while(true)
+    % User interface
+    disp('Please, input your option:');
+    disp('     1 - Count objects;');
+    disp('     2 - Show centroids;');
+    disp('     3 - Show perimeters;');
+    disp('     4 - Show areas;');
+    disp('     5 - Show distance between objects;');
+    disp('     6 - Show similar objects;');
+    disp('     7 - Exit.');
+    disp(' ');
+    option = input('Your option: ');
+
+    % Program's flow
+    switch option
+        case 1
+            disp('The number of objects in the image is:');
+            disp(length(objects));
+        case 2
+            close all;
+            disp('Missing implementation')
+        case 3
+            close all;
+            disp('Missing implementation')
+        case 4
+            disp('Missing implementation')
+        case 5
+            disp('Missing implementation')
+        case 6
+            close all;
+            disp('Missing implementation')
+        case 7
+            disp('Googbye.');
+            break;
+    end
+end
