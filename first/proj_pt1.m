67f0a98cebf47e4b1926c35ff475eb629be665ba
Tomás Alves <sir.mastermind94@gmail.com>
2016-04-26 14:59:10 +0100

    Added more functionalities.

--- first/proj_pt1.m
+++ first/proj_pt1.m
@@ -91,11 +91,11 @@ bw = im2bw(image_gray, graythresh(image_gray));
 % Use closure to get the shapes well defined
 se = strel('disk', 3);
 bw_final = imclose(bw,se);
-bw_final = bwmorph(bw_final,'hbreak'); % Removes H-connected pixels.
-bw_final = bwmorph(bw_final,'spur'); % Removes spur pixels.
-bw_final = bwmorph(bw_final,'clean'); % Removes isolated pixels (individual 1s that are surrounded by 0s).
-bw_final = bwmorph(bw_final,'remove'); % Removes interior pixels.
-bw_final = imfill(bw_final,'holes'); % Fills the regions.
+bw_final = bwmorph(bw_final,'hbreak'); % Removes H-connected pixels
+bw_final = bwmorph(bw_final,'spur'); % Removes spur pixels
+bw_final = bwareaopen(bw_final, 20); % Remove all object containing fewer than 20 pixels
+bw_final = bwmorph(bw_final,'remove'); % Removes interior pixels
+bw_final = imfill(bw_final,'holes'); % Fills the regions
 %figure,imshow(bw_final);
 
 % Find and label the different regions
@@ -263,6 +263,8 @@ while(true)
     disp('     22 - Show gradient magnitude and direction;');
     disp('     23 - Show skeleton of the unprocessed binary image;');
     disp('     24 - Show skeleton of the processed binary image;');
+    disp('     25 - Find circles with specific radius;');
+    disp('     26 - Find round objects;');
     disp('     0 - Exit.');
     disp(' ');
     option = input('Your option: ');
@@ -680,6 +682,48 @@ while(true)
         case 24
             close all;
             figure('Name','Skeleton of the processed binary image','NumberTitle','off'), imshow(bwmorph(bw_final,'skel',Inf));
+        case 25
+            close all;
+            min_r = input('Minimum radius: ');
+            max_r = input('Maximum radius: ');
+            [centers, radii, metric] = imfindcircles(image_gray,[min_r max_r]);
+            figure('Name','Circles found','NumberTitle','off'), imshow(image_gray);
+            hold on;
+            viscircles(centers, radii,'EdgeColor','b');
+            disp(divider);
+        case 26
+            close all;
+            [B,L] = bwboundaries(bw_final,'noholes');
+            % Display the label matrix and draw each boundary
+            figure('Name','Round objects','NumberTitle','off');
+            imshow(label2rgb(L, @jet, [.5 .5 .5])); % Jet for 64 colors
+            hold on;
+            for k = 1:length(B)
+              boundary = B{k};
+              plot(boundary(:,2), boundary(:,1), 'w', 'LineWidth', 2)
+            end
+            stats = regionprops(L,'Area','Centroid');
+            threshold = 0.90; % metric = 4*pi*area/perimeter^2
+            for k = 1:length(B) % loop over the boundaries
+              boundary = B{k}; % obtain (X,Y) boundary coordinates corresponding to label 'k'
+              % compute a simple estimate of the object's perimeter
+              delta_sq = diff(boundary).^2;
+              perimeter = sum(sqrt(sum(delta_sq,2)));
+              % obtain the area calculation corresponding to label 'k'
+              area = stats(k).Area;
+              % compute the roundness metric
+              metric = 4*pi*area/perimeter^2;
+              % display the results
+              metric_string = sprintf('%2.2f',metric);
+              % mark objects above the threshold with a black circle
+              if metric > threshold
+                centroid = stats(k).Centroid;
+                plot(centroid(1),centroid(2),'ko');
+              end
+              text(boundary(1,2),boundary(1,1),metric_string,'Color','black','FontSize',14,'FontWeight','bold');
+            end
+            title('Metrics closer to 1 indicate that the object is approximately round');
+            disp(divider);
         case 0
             close all;
             disp('Goodbye.');
