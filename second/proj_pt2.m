1e88ff2d98ce8ad5068c275f8e5260a087e005ca
Tomás Alves <sir.mastermind94@gmail.com>
2016-05-04 20:27:46 +0100

    Changed a few things. It's so slow!

--- second/proj_pt2.m
+++ second/proj_pt2.m
@@ -18,24 +18,20 @@ for k = 1 : 1 : nFrame/step
 end
 
 bkg = median(vid3D,3);
-figure; imagesc(uint8(bkg)); colormap gray;
+%figure; imagesc(uint8(bkg)); colormap gray;
 
 %ths = graythresh(bkg)*255;
-ths = 20;
+ths = 29;
 figure;
 for k = 1 : (nFrame/step - 1)
-    bw = (abs(vid3D(:, :, k) - bkg) > 29);
+    bw = (abs(vid3D(:, :, k) - bkg) > ths);
     bw_final = bwareaopen(bw, 70);
-    bw_final = bwmorph(bw_final,'hbreak'); % Removes H-connected pixels
-    bw_final = bwmorph(bw_final,'spur'); % Removes spur pixels
     se = strel('disk', 5);
     bw_final = imclose(bw_final,se);
     bw_final = imdilate(bw_final,se);
     se = strel('disk', 3);
     bw_final = imopen(bw_final,se);
     bw_final = bwareaopen(bw_final, 150);
-    bw_final = bwmorph(bw_final,'remove'); % Removes interior pixels
-    bw_final = imfill(bw_final,'holes'); % Fills the regions
     d(:, :, k) = bw_final;
     
     % Find and label the different regions
