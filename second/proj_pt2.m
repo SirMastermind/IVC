b2961e13ba755c0f0887c748bca72dde5558fe5f
Tomás Alves <sir.mastermind94@gmail.com>
2016-05-04 19:59:00 +0100

    Check this.
    Added bounding boxes.

--- second/proj_pt2.m
+++ second/proj_pt2.m
@@ -14,6 +14,7 @@ for k = 1 : 1 : nFrame/step
     str1 = sprintf(str,path,k,'jpg');
     img = imread(str1);
     vid3D(:,:,k) = rgb2gray(img);
+    
 end
 
 bkg = median(vid3D,3);
@@ -24,15 +25,37 @@ ths = 20;
 figure;
 for k = 1 : (nFrame/step - 1)
     bw = (abs(vid3D(:, :, k) - bkg) > 29);
-    bw_final = bwareaopen(bw, 75); % Remove all object containing fewer than 20 pixels
+    bw_final = bwareaopen(bw, 70);
     bw_final = bwmorph(bw_final,'hbreak'); % Removes H-connected pixels
     bw_final = bwmorph(bw_final,'spur'); % Removes spur pixels
     se = strel('disk', 5);
     bw_final = imclose(bw_final,se);
     bw_final = imdilate(bw_final,se);
+    se = strel('disk', 3);
+    bw_final = imopen(bw_final,se);
+    bw_final = bwareaopen(bw_final, 150);
     bw_final = bwmorph(bw_final,'remove'); % Removes interior pixels
     bw_final = imfill(bw_final,'holes'); % Fills the regions
     d(:, :, k) = bw_final;
-    imshow(bw_final); drawnow
+    
+    % Find and label the different regions
+    [lb, num]= bwlabel(bw_final);
+    %figure, imshow(mat2gray(lb));
+
+    % Get the stats of each label
+    stats = regionprops(lb);
+
+    % Compute area for each region
+    objects = [stats.Area];
+
+    if num > 0
+        for i = 1 : num % For each sorted index of the objects
+                    boundingBox = stats(i).BoundingBox; % Get the bounding box
+                    %imshow(bw_final);
+                    imagesc(uint8(vid3D(:, :, k))); colormap gray;
+                    rectangle('Position', boundingBox, 'EdgeColor','r', 'LineWidth', 3);
+                    drawnow, hold on;
+        end
+    end
     hold off;
 end
