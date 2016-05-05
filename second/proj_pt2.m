c97fe646cf36e0980c8d5e7edb9c644426a973c5
Tomás Alves <sir.mastermind94@gmail.com>
2016-05-05 14:28:14 +0100

    Distinguishing cars and people

--- second/proj_pt2.m
+++ second/proj_pt2.m
@@ -1,7 +1,8 @@
+%clc;
 close all;
-clear all;
+clear;
 
-path = 'DATASET1/TRAINING/CAMERA1_JPEGS/';
+path = 'DATASET1/TESTING/CAMERA1_JPEGS/';
 frameIdComp = 4;
 str = ['%s%.' num2str(frameIdComp) 'd.%s'];
 
@@ -14,29 +15,42 @@ for k = 1 : 1 : nFrame/step
     str1 = sprintf(str,path,k,'jpg');
     img = imread(str1);
     vid3D(:,:,k) = rgb2gray(img);
-    
 end
 
 bkg = median(vid3D,3);
 %figure; imagesc(uint8(bkg)); colormap gray;
 
-%ths = graythresh(bkg)*255;
 ths = 29;
-figure;
-for k = 1 : (nFrame/step - 1)
+
+bw = (abs(vid3D(:, :, 1) - bkg) > ths);
+bw_final = bwareaopen(bw, 70);
+se = strel('disk', 5);
+bw_final = imclose(bw_final,se);
+se = strel('disk', 8);
+bw_final = imdilate(bw_final,se);
+se = strel('disk', 3);
+bw_final = imopen(bw_final,se);
+bw_final = bwareaopen(bw_final, 150);
+d(:, :, 1) = bw_final;
+previous_bw = bw_final;
+
+for k = 2 : (nFrame/step - 1)
     bw = (abs(vid3D(:, :, k) - bkg) > ths);
     bw_final = bwareaopen(bw, 70);
     se = strel('disk', 5);
     bw_final = imclose(bw_final,se);
+    se = strel('disk', 8);
     bw_final = imdilate(bw_final,se);
     se = strel('disk', 3);
     bw_final = imopen(bw_final,se);
     bw_final = bwareaopen(bw_final, 150);
-    d(:, :, k) = bw_final;
-    
+    d(:, :, k) = bw_final + previous_bw;
+    previous_bw = bw_final;
+end
+
+for k = 1 : size(d,3)
     % Find and label the different regions
-    [lb, num]= bwlabel(bw_final);
-    %figure, imshow(mat2gray(lb));
+    [lb, num]= bwlabel(d(:, :, k));
 
     % Get the stats of each label
     stats = regionprops(lb);
@@ -44,14 +58,20 @@ for k = 1 : (nFrame/step - 1)
     % Compute area for each region
     objects = [stats.Area];
 
+    imagesc(uint8(vid3D(:, :, k))); colormap gray; hold on;
     if num > 0
-        for i = 1 : num % For each sorted index of the objects
-                    boundingBox = stats(i).BoundingBox; % Get the bounding box
-                    %imshow(bw_final);
-                    imagesc(uint8(vid3D(:, :, k))); colormap gray;
-                    rectangle('Position', boundingBox, 'EdgeColor','r', 'LineWidth', 3);
-                    drawnow, hold on;
+        for i = 1 : num
+            boundingBox = stats(i).BoundingBox;
+            if (abs(boundingBox(3)/boundingBox(4) - 1) < 0.09)
+                continue;
+            end
+            if (boundingBox(3)/boundingBox(4) > 1)
+                rectangle('Position', boundingBox, 'EdgeColor','r', 'LineWidth', 2);
+            else
+                rectangle('Position', boundingBox, 'EdgeColor','b', 'LineWidth', 2);
+            end
         end
     end
+    drawnow;
     hold off;
 end
